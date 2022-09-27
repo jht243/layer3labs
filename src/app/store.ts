ada7ff8b832c9c5dbdca024f97a4f47e3fa7cf66
Dmitriy Blyndaruk <dmitriy.blyndaruk@gmail.com>
2022-09-27 14:29:09 +0300

    feat: remove store auth

--- src/app/store.ts
+++ src/app/store.ts
@@ -4,20 +4,20 @@ import {
   ThunkAction,
 } from '@reduxjs/toolkit';
 
-import { authApi } from './services/AuthService';
-import { userApi } from './services/UserService';
+// import { authApi } from './services/AuthService';
+// import { userApi } from './services/UserService';
 import auth from './slices/authSlice';
 
 export const store = configureStore({
   reducer: {
     auth,
-    [authApi.reducerPath]: authApi.reducer,
-    [userApi.reducerPath]: userApi.reducer,
+    // [authApi.reducerPath]: authApi.reducer,
+    // [userApi.reducerPath]: userApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
       getDefaultMiddleware()
-      .concat(authApi.middleware)
-      .concat(userApi.middleware),
+      // .concat(authApi.middleware)
+      // .concat(userApi.middleware),
 });
 
 export type AppDispatch = typeof store.dispatch;
